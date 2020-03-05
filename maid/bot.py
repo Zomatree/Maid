4c58a2efd436500a4b08af6df0b0ac2c23ec52fb
Zomatree <angelokontaxis@hotmail.com>
2020-03-05 20:48:29 +0000

    put custom commands in a cog, formats now inside the config, on_ready runs once

--- maid/bot.py
+++ maid/bot.py
@@ -2,51 +2,40 @@ import asyncio
 import discord
 from . import utils, config
 from discord.ext import commands, flags
-import typing
-import datetime
 import re
 import random
-import logging
 from inspect import isawaitable
 
-logging.basicConfig(level=logging.INFO)
+class CustomCommands(commands.Cog, name="Custom commands"):
+    pass
 
 
 class Bot(commands.Bot):
     session = utils.database.DatabaseHandler(**config.database_config)
     cache = {}
-    regex = r"\{\{([^{}]+)\}\}"
-    group_regex = r"{{\$([1-9][0-9]*)}}"
-    allowed_attrs = {
-        r"{{content}}": lambda ctx, match: ctx.message.content[len(ctx.prefix)+len(ctx.command.name):],
-        r"{{author_mention}}": lambda ctx, match: ctx.author.mention,
-        r"{{author_name}}": lambda ctx, match: ctx.author.name,
-        r"{{author_discriminator}}": lambda ctx, match: ctx.author.discriminator,
-        r"{{owner_mention}}": lambda ctx, match: f"<@{ctx.guild.owner_id}>",
-        r"{{owner_name}}": lambda ctx, match: ctx.guild.get_member(ctx.guild.owner_id).name,
-        r"{{owner_discriminator}}": lambda ctx, match: ctx.guild.get_member(ctx.guild.owner_id).discriminator,
-    }
-    special = {
-        r"{{(rnd|random)\((([^,],)*[^,]+),?\)}}": lambda ctx, match: random.choice(match.group(2).split(",")),
-    }
+    ran = False
 
     async def on_ready(self):
-        self.load_extension("jishaku")
-        self.load_extension("maid.custom")
-        rows = await self.session.get("commands", [], {})
-        for row in rows:
-            if row[1] not in self.cache:
-                self.cache[row[1]] = {}
-            self.cache[row[1]][row[0]] = [row[2], row[3]]
-            self.create_custom_command(row[1])
-        print("Ready!")
+        if not self.ran:
+            self.load_extension("jishaku")
+            self.load_extension("maid.custom")
+            self.add_cog(CustomCommands())
+            rows = await self.session.get("commands", [], {})
+            for row in rows:
+                if row[1] not in self.cache:
+                    self.cache[row[1]] = {}
+                self.cache[row[1]][row[0]] = [row[2], row[3]]
+                self.create_custom_command(row[1])
+            print("Ready!")
+        self.ran = True
 
     def create_custom_command(self, name):
-
-        @self.command(name=name)
-        @commands.check(lambda _ctx:  _ctx.guild.id in _ctx.bot.cache[_ctx.command.name])
-        async def _command(_ctx, *args):
-            await _ctx.send(await self.format_message(_ctx, "".join(args), self.cache[_ctx.command.name][_ctx.guild.id][0]))
+        if not self.get_command(name):
+            @self.command(name=name)
+            @commands.check(lambda _ctx:  _ctx.guild.id in _ctx.bot.cache[_ctx.command.name])
+            async def _command(_self, _ctx, *args):
+                await _ctx.send(await self.format_message(_ctx, "".join(args), self.cache[_ctx.command.name][_ctx.guild.id][0]))
+            _command.cog = self.get_cog("Custom commands")
 
     async def add_custom_command(self, guildid, name, returnstr, args):
         await self.session.insert("commands", [guildid, name, returnstr, args])
@@ -72,12 +61,12 @@ class Bot(commands.Bot):
 
         while True:
             found = False
-            match = re.search(self.regex, "".join(strformat))  # hello -> {{$1}} <-
+            match = re.search(config.regex, "".join(strformat))  # hello -> {{$1}} <-
             if not match or not "".join(strformat)[match.start(1):match.end(1)]:
                 break
 
             content = "".join(strformat)[match.start():match.end()]
-            arg = re.match(self.group_regex, content)  # checking for {{$1}}
+            arg = re.match(config.group_regex, content)  # checking for {{$1}}
             if arg:
                 try:
                     strformat[match.start():match.end()] = args[int(arg.group(1))-1]  # {{$1}} -> args[1-1] command arg indexes will start at 1 so need to minus 1
@@ -85,13 +74,13 @@ class Bot(commands.Bot):
                     pass  # if the number is more than the total amount of args, TODO: check this when creating the command
                 found = True
 
-            for regex, func in self.special.items():
+            for regex, func in config.special.items():
                 special_match = re.match(regex, content)
                 if special_match:
                     strformat[match.start(1):match.end(1)] = await self.call_func(func, ctx, special_match)  # replaces the stuff inside the {{}} so the return of the funcs can be dynamic.
                     found = True
 
-            for regex, func in self.allowed_attrs.items():
+            for regex, func in config.allowed_attrs.items():
                 attr_match = re.match(regex, content)  # checking for {{author_name}} or similar
                 if attr_match:
                     strformat[match.start():match.end()] = await self.call_func(func, ctx, attr_match)
